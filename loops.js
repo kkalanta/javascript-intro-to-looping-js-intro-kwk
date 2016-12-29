b24423657a979715dd26b0c45634dd3e02a03585
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-29 22:07:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,13 @@
+
+function forLoop(array) {
+  //var array = []; 
+  for (let i = 0; i < 25; i++) {
+    console.log(`"I am 1 strange loop${i === 1 ? '' : 's'}."`); 
+    console.log(`"I am ${i} strange loop${1 < i < 25 ? '' : 's'}."`)} 
+  
+  //if (i === 0) {console.log("I am 1 strange loop.")}
+  //else {console.log(`I am ${i} strange loops.`)}
+  return array  
+
+}
+
