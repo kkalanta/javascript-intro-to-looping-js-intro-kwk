ed5b6eb80f2d97fcc8437d8cdc941dafad38095b
Kimia Kalantari <kimiakalantari1356@gmail.com>
2017-01-01 21:16:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,7 @@
 
 // Question 1: For Loop 
 function forLoop(array) {
-  var array = []; 
-  for (let i = 0; i <= 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {console.log(`"I am 1 strange loop."`)}
     else {console.log(`"I am ${i} strange loops."`)}
   }
